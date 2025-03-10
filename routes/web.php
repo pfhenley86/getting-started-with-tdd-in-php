a61ce7348ada275f88ed6701d59de4752dcc6876
Ford Henley <info@fordswebhelp.com>
2025-03-10 17:17:16 -0500

    Revert "completed without tdd tutorial"
    This reverts commit b19296de893706ede84b3936aa07d07e2fd27cf4.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\PublishPodcastController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +14,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::post('podcasts/{podcast}/publish', PublishPodcastController::class)
-->name('podcasts.publish');
