4c0cdc444073fb3c228835b79d062e1b873da3ba
Ford Henley <info@fordswebhelp.com>
2025-03-12 10:30:25 -0500

    finished testing course

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PublishPodcastController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('podcasts/{podcast}/publish', PublishPodcastController::class)
+->name('podcasts.publish');
